e9a1b1cdd0907344a790118df28e5789f1fdb23f
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2012-12-28 14:39:15 +0100

    New reviewer list format: one page with templates

--- add_reviewer.py
+++ add_reviewer.py
@@ -2,52 +2,84 @@ import sys, json
 from pipes import quote
 import subprocess
 import pywikibot
+import lxml.objectify
 
 site = pywikibot.getSite('mediawiki', 'mediawiki')
 
-def get_reviewer_page_text(change):
-    return pywikibot.Page(site, 'Git/reviewers/' + change['project']).get().encode('utf-8')
+class ReviewerFactory(object):
+    def __init__(self, site=pywikibot.getSite('mediawiki', 'mediawiki'),
+                       page="Git/Reviewers",
+                       template="Gerrit-reviewer"):
+        self.site = site
+        self.page = page
+        self.template = template
 
-def get_reviewers_from_text(change, text):
-    number = int(change['number'])
-    reviewers = []
-    for i, line in enumerate(text.split('\n')):
+    @property
+    def data(self):
+        if hasattr(self, '_data'):
+            return self._data
+        return pywikibot.data.api.Request(action="parse", page=self.page, generatexml=True, site=self.site).submit()
+
+    @property
+    def objecttree(self):
+        return lxml.objectify.fromstring(self.data['parse']['parsetree']['*'])
+
+    def _tryParseInt(self, value, default=None):
         try:
-            line = line.strip()
-            if not line:
-                continue
-            if line.startswith('<') or line.startswith('#'):
-                continue
-            elif line.startswith('%'):
-                modulo, reviewer = line.split(' ', 1)
-                if (number + i) % int(modulo[1:]) == 0:
-                        reviewers.append(reviewer)
-            else:
-                reviewers.append(line)
+            return int(value)
         except Exception, e:
-            print e
-            pass
+            return default
 
-    return reviewers
+    def reviewer_generator(self, project):
+        tree = self.objecttree
 
-def test_get_reviewers_from_text():
-    change = {'number': '0'}
-    text = """<pre>
-# Comment line
-User always
-%2 User sometimes but not now
-%2 User sometimes right now
-"""
-    result = get_reviewers_from_text(change, text)
-    assert result == ["User always", "User sometimes right now"], result
+        for section in tree.iter('h'):
+            if section.text.strip('= ') != project:
+                continue
+            for sibling in section.itersiblings():
+                if sibling.tag == "h":
+                    break
+                if sibling.tag == "template" and sibling.title == self.template:
+                    reviewer = None; modulo = 1
+                    for part in sibling.iter('part'):
+                        if part.name == "" and part.name.attrib['index'] == '1':
+                            reviewer = part.value
+                        elif part.name == "every":
+                            modulo = self._tryParseInt(part.value, 1)
+                            if modulo < 2:
+                                modulo = 1
+                    yield reviewer, modulo
 
-def get_reviewers(change):
+def get_reviewers(change, RF=ReviewerFactory()):
     try:
-        text = get_reviewer_page_text(change)
-        return get_reviewers_from_text(change, text)
+        num = int(change['number'])
+        reviewers = []
+        for i, (reviewer, modulo) in enumerate(RF.reviewer_generator(change['project'])):
+            if ((num + i) % modulo == 0):
+                reviewers.append(reviewer)
+        return reviewers
     except Exception, e:
         print e
         return []
+
+def test_get_reviewers():
+    RF = ReviewerFactory()
+    RF._data = json.load(open("api_result"))
+    name = 'test/mediawiki/extensions/examples'
+
+    for i in range(5):
+        change = {'number': i, 'project': name}
+        revs = get_reviewers(change, RF)
+        if i % 5 == 0:
+            assert revs == ["Merlijn van Deen", "Sumanah"]
+        else:
+            assert revs == ["Sumanah"]
+
+    RF = ReviewerFactory()
+    for i in range(5):
+        change = {'number': i, 'project': name}
+        revs = get_reviewers(change, RF)
+        print name, i, revs
 
 if __name__ == "__main__":
     while True:
