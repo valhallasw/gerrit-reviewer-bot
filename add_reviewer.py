b05202fc702ce655c83a4f9cb5b25253191dede8
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2012-12-29 15:15:22 +0100

    set DOTALL | IGNORECASE for file regexp

--- add_reviewer.py
+++ add_reviewer.py
@@ -56,7 +56,7 @@ class ReviewerFactory(object):
                             if modulo < 2:
                                 modulo = 1
                         elif part.name == "file_regexp":
-                            filere = re.compile(part.value.text or part.value.ext.inner.text)
+                            filere = re.compile(part.value.text or part.value.ext.inner.text, flags=re.DOTALL | re.IGNORECASE)
                     if any(filere.match(file) for file in changedfiles):
                         yield reviewer, modulo
 
