a4463762fb8294a54f719fd30e13a673beb16e64
Merlijn van Deen <valhallasw+tools@gmail.com>
2022-04-16 21:19:28 +0000

    K8s job monitoring

---
+++ error_email_k8s.py
@@ -0,0 +1,40 @@
+import smtplib
+from email.message import EmailMessage
+
+errlog = open('gerrit-reviewer-bot.err').readlines()[-100:]
+
+errorlines = [l for l in errlog if "Running as task" not in l]
+
+if not any(errorlines):
+    print("OK")
+    exit(0)
+
+errortext = "".join(errorlines)
+
+message = f"""Hello valhallasw,
+
+It seems gerrit-reviewer-bot has encountered some trouble. Please see
+the error below:
+
+-----------------------------------------------------------------------
+{errortext}
+-----------------------------------------------------------------------
+
+The full error log has been attached for your convenience.
+
+With kind regards,
+valhallasw in the past
+"""
+
+msg = EmailMessage()
+msg.set_content(message)
+msg['Subject'] = "Gerrit Reviewer Bot broken"
+msg['From'] = "tools.gerrit-reviewer-bot <tools.gerrit-reviewer-bot@tools.wmflabs.org>"
+msg['To'] = "valhallasw@arctus.nl"
+msg.add_attachment("".join(errlog), filename='error.log')
+
+s = smtplib.SMTP('mail.tools.wmflabs.org')
+s.send_message(msg)
+s.quit()
+
+print("Sent error email")
