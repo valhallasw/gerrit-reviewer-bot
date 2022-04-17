d3d1e29e450a1dd0bc04f8dc87beb8601b822fa7
Merlijn van Deen <valhallasw@arctus.nl>
2022-04-17 11:00:37 +0200

    Update test_online.py

--- test/test_online.py
+++ test/test_online.py
@@ -10,9 +10,9 @@ basepath = pathlib.Path(__file__).parent
 
 def test_gerrit_and_reviewer_factory():
     gerrit = gerrit_rest.GerritREST('https://gerrit.wikimedia.org/r')
-    changeset = gerrit.get_changeset("491868")
+    changeset = gerrit.get_changeset("710665")
 
     RF = add_reviewer.ReviewerFactory()
     reviewers = RF.get_reviewers_for_changeset(changeset)
 
-    assert 'Merlijn van Deen' in reviewers
+    assert 'Merlijn van Deen - alternative' in reviewers
