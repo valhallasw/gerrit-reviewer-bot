9142877bcf08525b31451e785bfa0e8f3133485f
Siebrand Mazeland <siebrand@kitano.nl>
2026-08-29 11:37:49 +0200

    Remove obsolete files

--- error_email.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /data/project/gerrit-reviewer-bot
-(tail gerrit_reviewer_bot.err -n 100 | grep -v "Running as task") && (tail gerrit_reviewer_bot.err -n 100 | mail -s "Gerrit Reviewer Bot broken" valhallasw@arctus.nl)
--- prune.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-tail -n 10000 gerrit_reviewer_bot.out > out.tmp
-mv out.tmp gerrit_reviewer_bot.out
-
-tail -n 10000 gerrit_reviewer_bot.err > err.tmp
-mv err.tmp gerrit_reviewer_bot.err
