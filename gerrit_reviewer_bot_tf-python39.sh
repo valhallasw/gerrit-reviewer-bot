f2a5173dde2d7427ea2ef8326fe227bd384a7862
Siebrand Mazeland <siebrand@kitano.nl>
2026-08-29 11:13:31 +0200

    Fix SSH host key changes breaking the bot
    Bug: T434075

--- gerrit_reviewer_bot_tf-python39.sh
+++ gerrit_reviewer_bot_tf-python39.sh
@@ -6,6 +6,8 @@ echo `date`: Running as task $HOSTNAME
 echo `date`: Running as task $HOSTNAME >& 2
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+curl -sSf https://config-master.wikimedia.org/known_hosts -o known_hosts
+
 export PYTHONIOENCODING=utf-8
 timeout 1h $HOME/venv-tf-python39/bin/python pop3bot.py
 
